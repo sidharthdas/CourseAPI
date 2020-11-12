c742fd9d8617103962a2c64ea5bc2488ff2f1128
Sidharth <sidharthdas.1995@gmail.com>
2020-11-12 21:23:28 +0530

    Added ExceptionError class

---
+++ course-api/src/main/java/com/course/courseapi/core/ExceptionErrors.java
@@ -0,0 +1,19 @@
+package com.course.courseapi.core;
+
+public class ExceptionErrors {
+	
+		// Error Codes
+		public static final String NULLOBEJCTERRORCODE = "1001";
+		public static final String OBJECTALREADYPRESENTCODE = "1002";
+	
+		//Error Messages
+		public static final String NULLOBJECTERRORMESSAGE = "Object is Null";
+		public static final String OBJECTALREADYPRESENTMESSAGE = "Object is Present";
+		
+	
+		//Error Desc
+		public static final String NULLOBJECTERRORDESC = "Object is Null comming from Front-end";
+		public static final String OBJECTALREADYPRESENTDESC = "Object is present in the DB. Duplicate entry not allowed. ";
+
+
+}
